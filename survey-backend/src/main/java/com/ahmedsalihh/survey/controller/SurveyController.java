979f3490c29863a3459cbd841ae0dc7b2e6f1f7a
ahmedsalihh <ahmedsalihh@gmail.com>
2020-08-23 04:59:26 +0300

    Add create survey form

--- survey-backend/src/main/java/com/ahmedsalihh/survey/controller/SurveyController.java
+++ survey-backend/src/main/java/com/ahmedsalihh/survey/controller/SurveyController.java
@@ -2,7 +2,6 @@ package com.ahmedsalihh.survey.controller;
 
 import com.ahmedsalihh.survey.model.Survey;
 import com.ahmedsalihh.survey.service.SurveyService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- survey/src/component/CreateSurvey/index.js
+++ survey/src/component/CreateSurvey/index.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const CreateSurvey = () => {
-  return <h1>Create survey</h1>;
+  let history = useHistory();
+
+  const [topic, setTopic] = useState('');
+  const [question, setQuestion] = useState('');
+  const [errMessage, setErrMessage] = useState(null);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (topic === '' || question === '') {
+      setErrMessage('Fileds cannot be empty!!');
+    } else {
+      Axios.post(`http://localhost:8080/surveys`, { topic, question })
+        .then(() => {
+          setQuestion('');
+          setTopic('');
+          setErrMessage(null);
+          history.push('/');
+        })
+        .catch(err => {
+          setErrMessage(err.message);
+        });
+    }
+  };
+  return (
+    <form className='form' onSubmit={handleSubmit}>
+      <div className='form-title'>Create Survey</div>
+      <div className='form-item'>
+        <label htmlFor='topic' className='form-label'>
+          Topic
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          name='topic'
+          id='topic'
+          value={topic}
+          placeholder='Enter Your Topic'
+          onChange={e => setTopic(e.target.value)}
+        />
+      </div>
+      <div className='form-item'>
+        <label htmlFor='topic' className='form-label'>
+          Question
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          name='question'
+          id='question'
+          value={question}
+          placeholder='Enter Your Question'
+          onChange={e => setQuestion(e.target.value)}
+        />
+      </div>
+      <div className='form-item'>
+        <button className='form-btn' type='submit'>
+          Create
+        </button>
+      </div>
+      {errMessage ? (
+        <div className='form-item'>
+          <span className='form-error'>{errMessage}</span>
+        </div>
+      ) : null}
+    </form>
+  );
 };
 
 export default CreateSurvey;
